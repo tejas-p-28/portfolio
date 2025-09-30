1a31bafcc3c3c04983db7bb8dd8a22c67177a964
Tejas Patil <tejas.patiltp2802@gmail.com>
2025-09-30 09:47:32 +0530

    requirement.txt error resolved

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tbd3t3n4uu(-r!am4gx@i#l30s_p+sk4m9h_3f!$x+lsj=(4na"
+SECRET_KEY = ""
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
